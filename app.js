324fa2b3eb837517818601bccba9aa0580ef5d0a
Mariana Minoni <marianaminoni@gmail.com>
2024-01-10 09:35:00 -0300

    subida desafio 3

---
+++ app.js
@@ -0,0 +1,74 @@
+
+const productManager = require('./ProductManager');
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/ping', (req, res) => {
+  res.send('pong');
+  
+ 
+ 
+})
+
+
+
+app.get('/products', async (req, res) => {
+
+  if(req.query.limit){
+
+    //envio al usuario la cantidad de productos que especifica
+    
+    res.send(productManager.limiteProductos(req.query.limit))
+  
+  } else{
+
+    
+    try {
+      // Llama a traerProductos y espera a que la promesa se resuelva
+      let productos = await productManager.traerProductos();
+      
+      //envio todos los productos usuario
+      res.send(productos);
+  } catch (error) {
+    
+      console.error(error);
+      res.status(500).send('Error interno del servidor');
+  }
+
+  }
+
+ 
+
+ 
+});
+
+app.get('/products/:id', async (req, res) => {
+
+  const id = req.params.id;
+
+  try {
+    const productId= parseInt(id);
+    
+    let productoPorId =  await productManager.getProductbyId(productId);
+    
+    // envio la respuesta con los productos
+    res.send(productoPorId);
+} catch (error) {
+    console.error(error);
+    res.status(500).send('Error interno del servidor');
+}
+});
+ 
+
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`aplicacion funcionando en puerto ${port}`)
+})
+
